9044ed29ee9ed6c8c942d8366208821682418bc6
Ryan Fisher <ryancfisher@gmail.com>
2013-04-01 22:46:54 -0700

    Initial commit

---
+++ js/stacyhaskell.js
@@ -0,0 +1,31 @@
+$(function() {
+    $('.thumbnail_link').fancybox({
+        'speedIn': 400,
+        'titlePosition': 'inside'
+    });
+    $('#boddingtons').fancybox({
+        'speedIn': 400,
+        'title': "<div style=\"text-align: left;\">CLIENT: Boddington's  OBJECTIVE: design a copy driven print ad  SOLUTION: British lingo and humor engages the viewer in fun tips<br /><br />Tourist Mistake #18 Don't order any posh cocktails at the pub. The locals will think you're off your head. Even if you slip the barkeep a tenner, he'll tell you to bugger off. Stick to a pint, you wanker! Always ask for Boddington's.</div>",
+        'titlePosition': 'inside'
+    });
+    $('#kohtao').fancybox({
+        'title': "<div style=\"text-align: left;\">CLIENT: KohTao.com  OBJECTIVE: create brand identity through a print ad campaign  SOLUTION: intriguing, hidden copy leaves the viewer curious and directs them to the website<br /><br />Here you are the only tourist and cameras are taboo. There is no need to go incognito, your presence will remain concealed. We do not believe in sharing chlorine or even sand for that matter. You don't need to be famous to enjoy exclusive privacy, just properly informed. KohTao.com </div>",
+        'titlePosition': 'inside'
+    });
+    $('#meadlb').fancybox({
+        'title': '<div style="text-align: left;">CLIENT: Mead  OBJECTIVE: create brand identity through a print ad campaign  SOLUTION: simple images representing milestones, created within the shape of the envelope, portrays the product as a functional and timeless keepsake </div>',
+        'titlePosition': 'inside'
+    });
+    $('#qtip').fancybox({
+        'title': "<div style=\"text-align: left;\">CLIENT: Q-Tips  OBJECTIVE: design a conceptually driven print ad  SOLUTION: portrayal of the product's softness without using words</div>",
+        'titlePosition': 'inside'
+    });
+    $('#northface').fancybox({
+        'title': "<div style=\"text-align: left;\">CLIENT: North Face  OBJECTIVE: design a conceptually driven print ad  SOLUTION: emphasizes the product's purpose and quality</div>",
+        'titlePosition': 'inside'
+    });
+    $('#miraclegro').fancybox({
+        'title': '<div style="text-align: left;">CLIENT: Miracle Gro  OBJECTIVE: design a conceptual print ad  SOLUTION: using humor to exaggerate the ability of the product</div>',
+        'titlePosition': 'inside'
+    });
+});
